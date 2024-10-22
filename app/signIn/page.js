7409e8ee03c39523e9a3cad6169e723e30a4b86c
greatrifat <greatrifat@gmail.com>
2024-10-23 03:32:11 +0600

    minor 2

--- app/signIn/page.js
+++ app/signIn/page.js
@@ -1,11 +1,11 @@
-import SignInComponent from '@/components/auth/signin'
+
 import React from 'react'
 
 export default function SignIn() {
   return (
     <div >
        
-       <SignInComponent/>
+      hello check
 
     </div>
   )
